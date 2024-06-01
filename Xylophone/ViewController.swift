23e3eb24192a290d9b9fe537be72e48be42fb3af
lazyproperty <lazyproperty@icloud.com>
2024-06-01 21:43:44 +0300

    some changes

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        let note = (sender.currentTitle)!
+        
+        play(sound: note)
+        sender.alpha = 0.5
+        
+        //задержка
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            sender.alpha = 1
+        }
+                                      
+        print("The note is \"\((sender.currentTitle)!)\"")
+    }
     
-
+    // create a func to play the sound
+    
+    func play(sound: String) {
+        
+      let url = Bundle.main.url(forResource: sound , withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+      }
+    
 }
 
